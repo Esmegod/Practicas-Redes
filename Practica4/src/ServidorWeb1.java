7b6f69f39298ae8bdcd1005b5b69100f73a1a51e
Esmeralda Godinez <egodinezm1600@alumno.ipn.mx>
2022-05-21 21:57:40 -0500

    DELETE terminado

--- Practica4/src/ServidorWeb1.java
+++ Practica4/src/ServidorWeb1.java
@@ -72,10 +72,8 @@ public class ServidorWeb1{
                         int bytes=0;
                         String mimePut = "";
                         while((token = st1.nextToken())!=null){
-
                             if(token.contains("Content-Length")){
                                 String[] s = token.split(":");
-                              
                                 System.out.println(s[1].trim());
                                 bytes = Integer.valueOf(s[1].trim());
                                 mimePut = st1.nextToken().split(":")[1].trim();
@@ -85,6 +83,7 @@ public class ServidorWeb1{
                         }
                         // Se inicia con la lectura del archivo
                         try{
+                            
                             String extension = mimePut.split("/")[1];
                             if(extension.equals("plain"))
                                 extension = "txt";
@@ -120,38 +119,84 @@ public class ServidorWeb1{
                         }finally{
                             dos.close();
                         }
-                    }
-                }else if(line.toUpperCase().startsWith("GET")){
-                        StringTokenizer tokens = new StringTokenizer(line,"?");
-                        String req_a = tokens.nextToken();
-                        String req = tokens.nextToken();
-                        System.out.println("Token1: "+req_a);
-                        System.out.println("Token2: "+req);
-                        String parametros = req.substring(0, req.indexOf(" "))+"\n";
-                        System.out.println("parametros: "+parametros);
-                        StringBuffer respuesta = new StringBuffer();
+                        
+                    }else if(line.toUpperCase().startsWith("DELETE")){
+                        try{
+                            getArch(line, st1);
+                            File f = new File("archivosRecibidos/"+FileName);
+                            if(f.exists() && f.canWrite()){
+                                f.delete();
+                                /***********************************************/
+                                String sb = "";
+                                sb = sb +"HTTP/1.0 202 accepted\n";
+                                sb = sb +"Server: Mesfer Server/1.0 \n";
+                                sb = sb +"Date: " + new Date()+" \n";
+                                sb = sb +"\n";
+                                dos.write(sb.getBytes());
+                                dos.flush();
+                                /***********************************************/
+                            }else{
+                                //Respuesta de error para eliminar
+                                /***********************************************/
+                                String sb = "";
+                                sb = sb +"HTTP/1.0 204 No content\n";
+                                sb = sb +"Server: Mesfer Server/1.0 \n";
+                                sb = sb +"Date: " + new Date()+" \n";
+                                sb = sb +"\n";
+                                dos.write(sb.getBytes());
+                                dos.flush();
+                                /***********************************************/
+                            }
+                        }catch(Exception e){
+                            e.printStackTrace();
+                             /***********************************************/
+                            String sb = "";
+                            sb = sb +"HTTP/1.0 500 error\n";
+                            sb = sb +"Server: Mesfer Server/1.0 \n";
+                            sb = sb +"Date: " + new Date()+" \n";
+                            sb = sb +"\n";
+                            dos.write(sb.getBytes());
+                            dos.flush();
+                            /***********************************************/
+                        }finally{
+                            dos.close();
+                        }
+                    }else if(line.toUpperCase().startsWith("HEAD")){
 
-                        respuesta.append("HTTP/1.0 200 Okay \n");
-                        String fecha= "Date: " + new Date()+" \n";
-                        respuesta.append(fecha);
-                        String tipo_mime = "Content-Type: text/html \n\n";
-                        respuesta.append(tipo_mime);
-                        respuesta.append("<html><head><title>SERVIDOR WEB</title></head>\n");
-                        respuesta.append("<body bgcolor=\"#AACCFF\"><center><h1><br>Parametros Obtenidos..</br></h1><h3><b>\n");
-                        respuesta.append(parametros);
-                        respuesta.append("</b></h3>\n");
-                        respuesta.append("</center></body></html>\n\n");
-                        System.out.println("Respuesta: "+respuesta);
-                        dos.write(respuesta.toString().getBytes());
-                        dos.flush();
-                        dos.close();
-                        socket.close();
-                }else{
-                        dos.write("HTTP/1.0 501 Not Implemented\r\n".getBytes());
-                        dos.flush();
-                        dos.close();
-                        socket.close();
+                    }
                 }
+                else if(line.toUpperCase().startsWith("GET")){
+                            StringTokenizer tokens = new StringTokenizer(line,"?");
+                            String req_a = tokens.nextToken();
+                            String req = tokens.nextToken();
+                            System.out.println("Token1: "+req_a);
+                            System.out.println("Token2: "+req);
+                            String parametros = req.substring(0, req.indexOf(" "))+"\n";
+                            System.out.println("parametros: "+parametros);
+                            StringBuffer respuesta = new StringBuffer();
+
+                            respuesta.append("HTTP/1.0 200 Okay \n");
+                            String fecha= "Date: " + new Date()+" \n";
+                            respuesta.append(fecha);
+                            String tipo_mime = "Content-Type: text/html \n\n";
+                            respuesta.append(tipo_mime);
+                            respuesta.append("<html><head><title>SERVIDOR WEB</title></head>\n");
+                            respuesta.append("<body bgcolor=\"#AACCFF\"><center><h1><br>Parametros Obtenidos..</br></h1><h3><b>\n");
+                            respuesta.append(parametros);
+                            respuesta.append("</b></h3>\n");
+                            respuesta.append("</center></body></html>\n\n");
+                            System.out.println("Respuesta: "+respuesta);
+                            dos.write(respuesta.toString().getBytes());
+                            dos.flush();
+                            dos.close();
+                            socket.close();        
+                    }else{
+                            dos.write("HTTP/1.0 501 Not Implemented\r\n".getBytes());
+                            dos.flush();
+                            dos.close();
+                            socket.close();
+                    }
+                
             }catch(Exception e){
                 e.printStackTrace();
             }
@@ -169,7 +214,7 @@ public class ServidorWeb1{
             }
             else if(line.toUpperCase().startsWith("POST")){
                 String token;
-                while((token = st.nextToken())!=null)
+                while((token = st.nextToken())!=null){
                     if(token.contains("imagen-abril")){
                         //Ttoken = imagen-abril=2022-04-19
                         String[] s = token.split("=");
@@ -177,8 +222,21 @@ public class ServidorWeb1{
                         mime = "image/jpeg";
                         break;
                     }
-                }
+                }    
+            }else if(line.toUpperCase().startsWith("DELETE")){
+                /*Datos: DELETE /archivo.pdf HTTP/1.1*/
+                i=line.indexOf("/");
+                f=line.indexOf(" ",i);
+                FileName=line.substring(i+1,f);
+            }else if(line.toUpperCase().startsWith("HEAD")){
+                // Checar
+                // i=line.indexOf("/");
+                // f=line.indexOf(" ",i);
+                // FileName=line.substring(i+1,f);
+                // if(line.contains(".pdf")) mime = "application/pdf";
+                // else mime = "text/html";
             }
+
         }
                         
         public void SendA(String arg, DataOutputStream dos1, String mime){
@@ -213,22 +271,24 @@ public class ServidorWeb1{
                 System.out.println(e.getMessage());
             }
         }
+
+
                         
-    
-		
-    public ServidorWeb1() throws Exception{
-        System.out.println("Iniciando Servidor.......");
-        this.ss=new ServerSocket(PUERTO);
-        System.out.println("Servidor iniciado:---OK");
-        System.out.println("Esperando por Cliente....");
-        for(;;){
-            Socket accept=ss.accept();
-            new Manejador(accept).start();
-        }
     }
+		
+        public ServidorWeb1() throws Exception{
+            System.out.println("Iniciando Servidor.......");
+            this.ss=new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado:---OK");
+            System.out.println("Esperando por Cliente....");
+            for(;;){
+                Socket accept=ss.accept();
+                new Manejador(accept).start();
+            }
+        }
     
     public static void main(String[] args) throws Exception{
         ServidorWeb1 sWEB=new ServidorWeb1();
     }
-	
+    
 }
